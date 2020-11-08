d5adc79c032405f9ceaa1234a658e88b2b3a4e8e
Peterson <pepe419@gmail.com>
2020-11-07 21:16:49 -0300

    Ajustado o splash e montado a estrutura da tela de login

---
+++ app/src/main/java/com/example/digitalhousefoods/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package com.example.digitalhousefoods
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Toast.makeText(this, "Splash", Toast.LENGTH_LONG).show()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            finish()
+            abrirTelaHome()
+        },3000)
+    }
+
+    fun abrirTelaHome(){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
