a6884ef5d815dbdda07efc61c2181251e2e8a8f7
Peterson <pepe419@gmail.com>
2020-11-08 01:30:38 -0300

    Criado a estrutura da tela de registro

--- app/src/main/java/com/example/digitalhousefoods/LoginActivity.kt
+++ app/src/main/java/com/example/digitalhousefoods/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.example.digitalhousefoods
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnTelaRegistro = findViewById<Button>(R.id.btnRegistrar)
+
+        btnTelaRegistro.setOnClickListener{
+            abrirTelaCadastroUsuario()
+        }
+    }
+
+    fun abrirTelaCadastroUsuario(){
+        val intent = Intent(this, CadastroUsuario::class.java)
+        startActivity(intent)
     }
 }
